89f10b3338adf9ec4a424898fbf16f17ba5a48a2
georgyv <georgy_varghese@hotmail.com>
2017-09-01 01:08:31 +0530

    Config File added & Alert Message Changed

--- config.js
+++ config.js
@@ -1,30 +1,7 @@
-var format = require('util').format;
 
- var config = {}
 
- config.raceCentreCollectionId = "RaceCentre";
- config.raceDetailsCollectionId = "RaceDetails";
- config.teamCollectionId = "Team";
- config.riderCollectionId = "Rider";
- config.raceStartListId = "RaceStartList";
- config.newsCollectionId = "News";
- config.raceResultId = "RaceResult";
- config.raceAnalyticsId = "RaceAnalytics";
- config.raceConfigId = "RaceConfig";
- config.featureCardsId = "FeatureCards";
- config.broadcastUserId = "BroadcastUser";
- config.userTrackerId = "UserTracker";
- config.raceSeriesConfigurationId = "RaceSeriesConfiguration"
- 
- config.mongodbVelon_poolSize=50;
- config.mongodbVelonUser_poolSize=10;
- 
- config.loggerEnable = true;
- config.loggerDebug = false;
- config.resourceCount = 13;
- 
- 
- config.useDummyLiveTime = false;
- config.dummyCurrentEpochTime = 1487929081;
+ var config = {} 
+
+ config.mobileNumber = '+919895282129'; 
  
  module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var lodash = require('lodash');
 var path = require('path');
 var router = express.Router();
+var config = require('../config');
 
 var accountSid = 'ACc57b0f4199a58e7aeadc94d0e012b572'; // Your Account SID from www.twilio.com/console
 var authToken = '97fd58f7c98833768fdc59eabbecaff5';   // Your Auth Token from www.twilio.com/console
@@ -16,57 +17,48 @@ var client = new twilio(accountSid, authToken);
 // set alerts 
 router.get('/sendalerts', function(req, res) {
 
-	//logger.infoLog("Get Races API");
-	console.log("Tested");
-	//console.log(client);
+	//console.log("Tested");	
+	try {			
+			var mobNumber = config.mobileNumber;			
+		
+			sendalert(mobNumber, function(response){				
+				res.json(response);				
+			})			   
 	
-	try {		
+	} catch (err) {
+		createErrorResponse(err, res);
+	}
+});
+
+
+function sendalert(mobNumber,callback) {	
+	
+		var response = {};
+				
 		client.messages.create({
-			body: 'Alert from SEW',
-			to: '+919895282129', // Text this number
-			//to: '+919037575291',
-			from: '+15106069752' // From a valid Twilio number
-		},function(err, data) {
+			body: 'Leak Confirmed',
+			to: mobNumber, // Text this number
+		   from: '+61429532014' // From a valid Twilio number
+		},function(err, data) {		
 			
-			var response = {};
 			if(err){				
-				//console.log(err);				
-				response.status = "Failure";
-				//response.message = err;		
-				
+				console.log(err);				
+				response.status = "Failed";
+				response.message = err.message;	
+				response.statusCode = err.status;				
 			} else {				
-				response.status = "Success";
-				response.message = data.body;	
-				response.from = data.from;
-				//console.log(data);							
-			}
-				
-			res.json(response);
-			//console.log(data);
-		});
+				response.status = "Success";					
+				response.From = data.from;
+				response.To = data.to;
+				response.message = data.body;				
+				console.log(data);							
+			}		
+			 console.log(response);
+			 callback(response);			
+			 return;
 			
-		
-		console.log("after");
-	
-	} catch (err) {
-
-		createErrorResponse(err, res);
-	}
-
-});
-
-// Get Race Info
-router.get('/race/:race_id', function(req, res) {
-
-	//logger.infoLog("Get Race Info API");
-
-	try {
-		
-	} catch (err) {
-		createErrorResponse(err, res);
-	}
-});
-
+		});		
+}
 
 // Log and create error messages to handle error events encountered in the code
 function createErrorResponse(err, res) {
@@ -84,7 +76,8 @@ function createErrorResponse(err, res) {
 			response.message = "Requested data cannot be found.";
 		}
 	}
-	logger.errorLog(response.message);
+	//logger.errorLog(response.message);
+	console.log(response.message)
 	res.json(response);
 }
 
